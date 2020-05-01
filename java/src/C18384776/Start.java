ce00e504aefd048394f988d72e74d6ddeb0f0f60
EduardSamul <hello@eduard.ie>
2020-05-01 12:34:14 +0100

    Music added. Screensaver looking visual added to the code.

--- java/src/C18384776/Start.java
+++ java/src/C18384776/Start.java
@@ -3,7 +3,8 @@ package C18384776;
 import ie.tudublin.Visual;
 
 public class Start extends Visual{
-
+    int menu = 0;
+    
     public void settings()
     {
         size(1000, 1000, P3D);
@@ -15,12 +16,17 @@ public class Start extends Visual{
         noCursor();
         setFrameSize(256);
         startMinim();
-        loadAudio("heroplanet.mp3");
-
+        loadAudio("Devochka S Kare.mp3");
     }
 
     public void draw()
     {
+        switch(menu) {
+            case 0:
+                drawCircleMoving();
+                break;
+        }
+
     }
 
     public void keyPressed()
@@ -29,7 +35,39 @@ public class Start extends Visual{
         {
             getAudioPlayer().cue(0);
             getAudioPlayer().play();
+        }       
+        
+        switch(key){
+            case '0':
+                drawCircleMoving();
+                break;
         }
     }
 
+    float sideways = 500;
+    float UpDown = 500;
+    float speedForX = 30;
+    float speedForY = 45;
+
+    public void drawCircleMoving()
+    {
+        noStroke();
+        fill(random(255), random(255), random(255));
+
+        // X Axis
+        sideways += speedForX;
+        if (sideways > width || sideways < 0)
+        {
+            speedForX *= -1;
+        }
+
+        // Y Axis
+        UpDown += speedForY;
+        if (UpDown > height || UpDown < 0)
+        {
+            speedForY *= -1;
+        }
+
+        circle(sideways, UpDown, 200);
+    }
 }
